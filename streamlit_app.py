c496fd4ee2f00f62bffdab40a839ce69a498bbc5
sakthi-91 <110520185+sakthi-91@users.noreply.github.com>
2022-08-03 16:22:40 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title('My New Snowflake training')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+stramlit.text('Kale,spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-range Eggs') 
+                
