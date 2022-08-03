512dd31a5a5027b041d1a4810afcb444699cba98
sakthi-91 <110520185+sakthi-91@users.noreply.github.com>
2022-08-03 16:23:35 +1000

    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
-stramlit.text('Kale,spinach & Rocket Smoothie')
+streamlit.text('Kale,spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-range Eggs') 
                 
